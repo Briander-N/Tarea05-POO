9d84fbbe1dc3c30b4b2abc1bcc86faf8d7816592
Briander <pochitobriander@gmail.com>
2026-04-19 13:59:20 -0500

    Ejercicio1 terminado

--- Ejercicio1/src/Estudiante.java
+++ Ejercicio1/src/Estudiante.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class Estudiante {
+    String nombre;
+    String carrera;
+    int edad;
+
+    void registrarDatos(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ingrese nombre: ");
+        nombre = sc.nextLine();
+
+        System.out.print("Ingrese carrera: ");
+        carrera = sc.nextLine();
+
+        System.out.print("Ingrese edad: ");
+        edad = sc.nextInt();
+
+    }
+    void mostrarDatos(){
+        System.out.println("----- DATOS DEL ESTUDIANTE -----");
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Carrera: " + carrera);
+        System.out.println("Edad: " + edad);
+    }
 }
